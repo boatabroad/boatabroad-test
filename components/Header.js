8369914d02a6394d101d38758fa4ffa2945a8ef1
Oscar Estrada <arturostrada2020@gmail.com>
2021-12-16 09:38:27 -0600

    banner, favicon and loco paths changed

--- components/Header.js
+++ components/Header.js
@@ -55,7 +55,7 @@ function Header({ placeholder }) {
         className="flex lg:hidden relative items-center h-9 cursor-pointer my-auto">
 
         <Image
-          src="/../public/img/isotipo.svg"
+          src="/img/isotipo.svg"
           layout="fill"
           objectFit="contain"
           objectPosition="left"
@@ -65,7 +65,7 @@ function Header({ placeholder }) {
         onClick={() => router.push("/")}
         className="hidden lg:flex relative items-center h-5 cursor-pointer my-auto">
         <Image
-          src="/../public/img/logo.svg"
+          src="/img/logo.svg"
           layout="fill"
           objectFit="contain"
           objectPosition="left"
