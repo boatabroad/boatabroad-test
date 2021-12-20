3f02dfde6704dafa27151b107f5bbfb2417f421d
Oscar Estrada <arturostrada2020@gmail.com>
2021-12-20 10:40:44 -0600

    map marker changed

--- components/Map.js
+++ components/Map.js
@@ -19,7 +19,7 @@ function Map({ searchResults }) {
     height: '100%',
     latitude: center.latitude,
     longitude: center.longitude,
-    zoom: 11,
+    zoom: 9,
   });
 
   return (
@@ -41,7 +41,7 @@ function Map({ searchResults }) {
               role='img'
               onClick={() => setSelectedLocation(result)}
               className='cursor-pointer text-2xl animate-bounce'
-              aria-label='push-pin'>📌
+              aria-label='push-pin'><svg xmlns="http://www.w3.org/2000/svg" width="13.976" height="17.118" viewBox="0 0 13.976 17.118"><path d="M6780.57,912.185l5.516-12.4v12.4Z" transform="translate(-6779.658 -899.787)" fill="#00bfc1" /><path d="M6778.989,923.525h6.428v3.417h-3.5Z" transform="translate(-6778.989 -909.824)" fill="#00bfc1" /><path d="M6797.244,913.89h-4.967V903.817Z" transform="translate(-6784.608 -901.491)" fill="#00bfc1" /><path d="M6798.584,923.525l-2.775,3.417h-3.532v-3.417Z" transform="translate(-6784.608 -909.824)" fill="#00bfc1" /></svg>
             </p>
           </Marker>
 
