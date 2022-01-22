afe0867e35ae7b7f8ba87382144f2c4cad5dc955
Leandro Cotti <lefcott@hotmail.com>
2022-01-22 16:47:46 -0300

    removed log

--- pages/api/payments/events/index.ts
+++ pages/api/payments/events/index.ts
@@ -6,7 +6,6 @@ export default function (req: VercelRequest, res: VercelResponse) {
     console.log('req.headers', req.headers);
 
     return res.json({ message: 'thanks!' });
-    // return createPayment(req, res);
   }
 
   res.status(405).json({ error: 'Method not allowed' });
